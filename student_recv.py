5c4b5a1b3b0009c091bae5a2a45ceb7b75fad622
Tedu <lvze@tedu.cn>
2019-05-22 14:33:23 +0800

    add student_recv

--- student_recv.py
+++ student_recv.py
@@ -9,13 +9,5 @@ st =struct.Struct("i32sif")
 # 打开文件
 f = open("student.txt",'a')
 
-while  True:
-    data,addr =s.recvfrom(1024)
-    data =st.unpack(data)
-    print(data)
-    info = "%d  %s  %d  %.2f\n"%(data[0],data[1].decode(),data[2],data[3])
-    f.write(info)
-    print(info)
-
-s.close
-f.close
+for i in range(5):
+    print(i)
